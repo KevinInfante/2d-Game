1053b8ea6d69efff948b2d13308aacb9d3cf191e
Kevin Infante <kevininfante90@gmail.com>
2024-10-22 22:25:21 -0400

    Replaced the default windows proc function with custom function LRESULT CALLBACK windows_window_callback(...). This callback function processes messages, basically inputs from the user. So far, no added functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,26 @@ static HWND window; //has to be global so that update_window() can access it
 // ############################################################
 //                        PLATFORM IMPLEMENTATION
 // ############################################################
+LRESULT CALLBACK windows_window_callback(HWND window, UINT msg, 
+                                        WPARAM wParam, LPARAM lParam)
+{
+    LRESULT result = 0;
+    
+    switch(msg){
+        case WM_CLOSE:{
+            running = false;
+            break;
+        }
+        default:{
+            // let window handle default input for now
+            //invoke the default proc address for every other msg
+            result = DefWindowProcA(window, msg, wParam, lParam);
+        }
+    }
+    //"now you know how sdl2 or glfw open up windows"
+    return result;
+}
+
 bool platform_create_window(int width, int height, char* title){
     HINSTANCE instance = GetModuleHandleA(0); //why use the A suffix?
     // the a stands for ansi which is the encoding...
@@ -41,10 +61,9 @@ bool platform_create_window(int width, int height, char* title){
     wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.lpszClassName = classID; //this is NOT the title, just a unique identifier
-    wc.lpfnWndProc = DefWindowProcA; // "long function pointer to a windows proc address"
-     //"this function is the callback for input, e.g. whenever you move the mouse,press keys, etc."
+    wc.lpfnWndProc = windows_window_callback; //Callback for input into the window
+        // lpfnwndproc "long function pointer to a windows procedure address"
         //all of those are events that will be sent to this function
-        //for now we use the default fnc, wince we're just opening a window
     if(!RegisterClassA(&wc)){ //if this succeeds, then we will have registered our class
         return false;
     }
@@ -89,7 +108,7 @@ int main(){
     platform_create_window(1200, 700, "Kevin Engine");
     while(running){
         //update
-        platform_update_window();
+        platform_update_window();//every loop iteration, we update the window,& we check for input
     }
     return 0;
 }
